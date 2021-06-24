9d9f7b62402850ffe7528dac09bb9b2037e5056b
negbook <negbook@gmail.com>
2021-06-24 21:13:29 +0800

    perfect

--- arrival.lua
+++ arrival.lua
@@ -1,138 +1,12 @@
 Arrival = {}
 Arrival.zonedata_full = {}
 Arrival.currentzonedata = {}
-Arrival.currentfocusdata = FlowDetector.Create('currentfocusdata',{})
+
 
 Arrival.ped = nil
-Arrival.pedcoords = FlowDetector.Create('coords',vector3(0.0,0.0,0.0)) 
-Arrival.nearzones = FlowDetector.Create('zones',{})
-Arrival.inzone = FlowDetector.Create('inzone',false)
-Arrival.currentarrivaldata_enter  = FlowDetector.Create('currentarrivaldata_enter',{})
-Arrival.currentarrivaldata_exit  = FlowDetector.Create('currentarrivaldata_exit',{})
-
-FlowDetector.Register("currentarrivaldata_enter",'change',function(name,old,new,isLinked)
-    if #new>0 then 
-        for i=1,#(new) do 
-            local v = new[i]
-            if v.arrival then v.arrival(v,'enter') end 
-        end 
-    end 
-end )
-FlowDetector.Register("currentarrivaldata_exit",'change',function(name,old,new,isLinked)
-    if #new>0 then 
-        for i=1,#(new) do 
-            local v = new[i]
-            if v.arrival  then v.arrival(v,'exit') end 
-        end 
-    end 
-end )
-FlowDetector.Link('coords','currentfocusdata')
-FlowDetector.Register("currentfocusdata",'change',function(name,old,new,isLinked)
-    if #new == 0 then 
-        if #Arrival.currentarrivaldata_enter>0 then 
-        Arrival.currentarrivaldata_exit = FlowDetector.Check('currentarrivaldata_exit',Arrival.currentarrivaldata_enter)
-        end 
-        Threads.KillLoopCustom('currentfocusdata',0)
-    end 
-    if Arrival.currentfocusdata and #Arrival.currentfocusdata > 0 then 
-        Threads.CreateLoopOnceCustom('currentfocusdata',0,function(delay)
-            local k = 1000
-            local cal = {1000}
-            local arrivaldataEnter = {} 
-            local arrivaldataExit = {} 
-            
-            for i=1,#Arrival.currentfocusdata do 
-                local v = Arrival.currentfocusdata[i]
-                local distance = v.distance
-                if distance < v.range then
-                    v.enter = true 
-                    table.insert(arrivaldataEnter,v)
-                    if v.exit~=nil and v.exit == true then 
-                        v.exit = false 
-                    end 
-                    table.insert(cal,distance)
-                else 
-                    if v.enter~=nil and v.enter == true then 
-                        v.enter = false 
-                        v.exit = true
-                        table.insert(arrivaldataExit,v)
-                    end 
-                    
-                end 
-                
-            end 
-            Arrival.currentarrivaldata_enter = FlowDetector.Check('currentarrivaldata_enter',arrivaldataEnter)
-            Arrival.currentarrivaldata_exit = FlowDetector.Check('currentarrivaldata_exit',arrivaldataExit)
-            local k = 332 + math.min(table.unpack(cal))*20
-            local waittime = k > 1000 and 1000 or k
-            delay.setter(waittime)
-        end,"freshfocus")
-        --Threads.SetLoopCustom("freshcurrentzone",0)
-        Threads.SetLoopCustom("freshfocus",0)
-    else 
-        if #Arrival.currentarrivaldata_enter>0 then 
-        Arrival.currentarrivaldata_exit = FlowDetector.Check('currentarrivaldata_exit',Arrival.currentarrivaldata_enter)
-        end 
-        Threads.KillLoopCustom('currentfocusdata',0)
-        
-    end 
-    
-end )
-FlowDetector.Register("inzone",'change',function(name,old,new,isLinked)
-    
-    if new == false then 
-        Arrival.currentzonedata = {}
-        if #Arrival.currentarrivaldata_enter>0 then 
-        Arrival.currentarrivaldata_exit = FlowDetector.Check('currentarrivaldata_exit',Arrival.currentarrivaldata_enter)
-        end 
-        Threads.KillLoopCustom('checkcurrentzonedatadistance',0)
-        Threads.KillLoopCustom('currentfocusdata',0)
-        Arrival.currentfocusdata = FlowDetector.Check('currentfocusdata',{})
-    else 
-        Arrival.currentzonedata = Arrival.zonedata_full[new]
-        if Arrival.currentzonedata and #Arrival.currentzonedata > 0 then 
-        Threads.CreateLoopOnceCustom('checkcurrentzonedatadistance',0,function(delay)
-            local k = 1000
-            local cal = {1000}
-            local arrivalfocusdata = {}
-            
-            for i=1,#Arrival.currentzonedata do 
-                local v = Arrival.currentzonedata[i]
-                local pos = vector3(v.x,v.y,v.z)
-                local distance = #(pos-Arrival.pedcoords)
-                if distance < 50.0 then 
-                    v.distance = distance
-                    table.insert(arrivalfocusdata,v)
-                    table.insert(cal,distance)
-                end 
-                
-            end 
-            
-            Arrival.currentfocusdata = FlowDetector.Check('currentfocusdata',arrivalfocusdata)
-            local k = (332 + math.min(table.unpack(cal))*20) * 4
-            local waittime = k > 4000 and 4000 or k
-            
-            delay.setter(waittime)
-        end,"freshcurrentzone")
-    end 
-    end 
-end )
-
-FlowDetector.Register("zones",'change',function(name,old,new,isLinked)
-    local zone = GetNameOfZone(Arrival.pedcoords)
-    Arrival.inzone =  FlowDetector.Check('inzone',Arrival.zonedata_full[zone] and zone or false)
-        --print(isLinked,name,old,new,json.encode(new))
-end )
-FlowDetector.Register("coords",'change',function(name,old,new,isLinked)
-    Arrival.nearzones =  FlowDetector.Check("zones", Arrival.getnearzones())
-end )
-
-local GetHashMethod = function(...)
-    local pack = {...}
-    result = GetNameOfZone(table.unpack(pack)) -- .. (id and tostring(id) or '')
-   
-    return result 
-end 
+Arrival.pedcoords = vector3(0.0,0.0,0.0)
+Arrival.pedzone = FlowDetector.Create('zone','')
+Arrival.debuglog = true
 
 
 Arrival.Register = function (datas,rangeorcb,_cb)
@@ -150,7 +24,7 @@ Arrival.Register = function (datas,rangeorcb,_cb)
         cb = rangeorcb 
     end 
     local data = Arrival.ConvertData(datas)  -- to .x .y .z .index 
-    local zonelist,zonedata = Arrival.CollectZoneData(data)
+    local zonelist,zonedata = Arrival.CollectZoneData(data,range)
     for i,v in pairs (zonedata) do 
         local zone = v.zone
         v.arrival = fntotable(cb)
@@ -158,27 +32,79 @@ Arrival.Register = function (datas,rangeorcb,_cb)
         if not Arrival.zonedata_full[zone] then Arrival.zonedata_full[zone]={} end 
         table.insert(Arrival.zonedata_full[zone],v)
     end 
-
     
+    if  GetCurrentResourceName() ~= resourceName or Arrival.debuglog then
+        Threads.CreateLoopOnce('inits',33,function(delay)
+            Arrival.ped = PlayerPedId()
+            Arrival.pedcoords = GetEntityCoords(Arrival.ped)
+            Threads.CreateThreadOnce(function()
+                FlowDetector.Register("zone",'change',function(name,old,new,isLinked)
+                    if old and #old>0 then 
+                        local zonedatas = Arrival.zonedata_full[old]
+                        if zonedatas and #zonedatas>0 then 
+                            for i=1,#zonedatas do 
+                                local v = zonedatas[i]
+                                local pos = vector3(v.x,v.y,v.z)
+                                local distance = #(pos-Arrival.pedcoords)
+                                if distance < v.range then
+                                    if not v.enter then 
+                                        v.enter = true 
+                                        if v.arrival then v.arrival(v,'enter') end 
+                                    end 
+                                    if v.exit~=nil and v.exit == true then 
+                                        v.exit = false 
+                                    end 
+                                    
+                                else 
+                                    if v.enter~=nil and v.enter == true then 
+                                        v.enter = false 
+                                        v.exit = true
+                                        if v.arrival then v.arrival(v,'exit') end 
+                                    end 
+                                    
+                                end 
+                            end 
+                        end 
+                    end 
+                end )
+            end)
+            Arrival.pedzone = FlowDetector.Check('zone',Arrival.GetHashMethod(Arrival.pedcoords.x,Arrival.pedcoords.y,Arrival.pedcoords.z,range)) 
+            local zonedatas = Arrival.zonedata_full[Arrival.pedzone]
+            if zonedatas and #zonedatas>0 then 
+                for i=1,#zonedatas do 
+                    local v = zonedatas[i]
+                    local pos = vector3(v.x,v.y,v.z)
+                    local distance = #(pos-Arrival.pedcoords)
+                    if distance < v.range then
+                        if not v.enter then 
+                            v.enter = true 
+                            if v.arrival then v.arrival(v,'enter') end 
+                        end 
+                        if v.exit~=nil and v.exit == true then 
+                            v.exit = false 
+                        end 
+                        
+                    else 
+                        if v.enter~=nil and v.enter == true then 
+                            v.enter = false 
+                            v.exit = true
+                            if v.arrival then v.arrival(v,'exit') end 
+                        end 
+                        
+                    end 
+                end 
+            end 
+        end)
+    end 
 end 
-CreateThread(function()
 
-    Arrival.ped = PlayerPedId()
-    Arrival.pedcoords = FlowDetector.Check('coords',GetEntityCoords(Arrival.ped)) 
-    Arrival.nearzones =  FlowDetector.Check('zones',Arrival.getnearzones())
-
-    Threads.CreateLoopCustom('inits',500,function(delay)
-        Arrival.ped = PlayerPedId()
-        Arrival.pedcoords = FlowDetector.Check("coords", GetEntityCoords(Arrival.ped))
-        local k = math.random(332,664)
-        delay.setter(k)
-    end)
-    
-    
-    
-end)
-
-Arrival.getnearzones = function()
+Arrival.getnearzones = function(...)
+    local _pos = {...}
+    if #{...} == 3 then 
+        _pos = vector3(_pos[1],_pos[2],_pos[3])
+    else 
+        _pos = _pos[1]
+    end 
     local nearzones = {}
     local included = function(zone) 
         local found = false 
@@ -189,37 +115,37 @@ Arrival.getnearzones = function()
         end 
         return found 
     end 
-    local pos = Arrival.pedcoords
+    
+        local pos = _pos
     local temp_y = 0.0
-    while included(GetHashMethod(pos)) do 
+  
         pos = GetObjectOffsetFromCoords(pos,0.0, 0.0, temp_y ,0.0)
         temp_y = temp_y + 8.0
-    end 
-    nearzones[1] = GetHashMethod(pos)
+    if not included(GetNameOfZone(pos)) then table.insert(nearzones,GetNameOfZone(pos)) end 
     local pos = Arrival.pedcoords
     local temp_x = 0.0
-    while included(GetHashMethod(pos)) do 
+    
         pos = GetObjectOffsetFromCoords(pos.x,pos.y,pos.z,0.0, temp_x, 0.0 ,0.0)
         temp_x = temp_x + 8.0
-    end 
-    nearzones[2] = GetHashMethod(pos)
+   
+    if not included(GetNameOfZone(pos)) then table.insert(nearzones,GetNameOfZone(pos)) end 
     local pos = Arrival.pedcoords
     local temp_y = 0.0
-    while included(GetHashMethod(pos)) do 
+
         pos = GetObjectOffsetFromCoords(pos,0.0, 0.0, temp_y ,0.0)
         temp_y = temp_y - 8.0
-    end 
-    nearzones[3] = GetHashMethod(pos)
+    
+    if not included(GetNameOfZone(pos)) then table.insert(nearzones,GetNameOfZone(pos)) end 
     local pos = Arrival.pedcoords
     local temp_x = 0.0
-    while included(GetHashMethod(pos)) do 
+ 
         pos = GetObjectOffsetFromCoords(pos,0.0, temp_x, 0.0 ,0.0)
         temp_x = temp_x - 8.0
-    end 
-    nearzones[4] = GetHashMethod(pos)
+ 
+    if not included(GetNameOfZone(pos)) then table.insert(nearzones,GetNameOfZone(pos)) end 
     return nearzones
 end 
-Arrival.CollectZoneData = function(datatable) --vector3 or {x=1.0,y=2.0,z=3.0}
+Arrival.CollectZoneData = function(datatable,range) --vector3 or {x=1.0,y=2.0,z=3.0}
     local isVector3 = type(datatable[1])=='vector3'
     local zonelist = {}
     local zonedata = {}
@@ -237,7 +163,7 @@ Arrival.CollectZoneData = function(datatable) --vector3 or {x=1.0,y=2.0,z=3.0}
     for i=1,#datatable do 
         local v = datatable[i]
         
-        local zone = GetHashMethod(v.x,v.y,v.z)
+        local zone = Arrival.GetHashMethod(v.x,v.y,v.z,range)
         table.insert(zonedata,{data=v.data,index=v.index,x=v.x,y=v.y,z=v.z,zone=zone})
         if not included(zone) then 
             table.insert(zonelist,zone) 
@@ -309,7 +235,14 @@ Arrival.ConvertData = function(datatable)
     return result --3 vector3,2 normal,1 .x .y .z
 end 
 
-
+Arrival.GetHashMethod = function(x,y,z,range)
+    local pos = vector3(x,y,z)
+    local range = range or 1.0
+    local range2 = range*4 > 50.0 and 50.0 or range*4
+    result = GetNameOfZone(pos) .. tostring(math.floor(GetHeightmapTopZForArea(pos.x-range,pos.y-range,pos.x+range,pos.y+range))) .. tostring(math.floor(GetHeightmapBottomZForArea(pos.x-range*2,pos.y-range*2,pos.x+range*2,pos.y+range*2))) .. tostring(math.floor(GetHeightmapBottomZForArea(pos.x-range2,pos.y-range2,pos.x+range2,pos.y+range2)))
+    --print(result)
+    return result 
+end 
 
 
 --debug 
